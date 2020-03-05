ea0126749c9a1f2e54b0ba3349ae99279f6d4bcb
Noland Seigler <nolandseigler@gmail.com>
2020-03-05 09:31:26 -0600

    another whiteboard and test

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -1,3 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Practice {
     public static double intDivideToReal (int a, int b) throws ArithmeticException {
         try {
@@ -19,5 +26,65 @@ public class Practice {
         } else {
             return String.format("%d > %d", a, b);
         }
+    }
+
+    /**
+     * Take a list of Integers and a target integer. Return any the index pairs from the array list that equal the target when summed. Example 1, 2, 3, 4 tgt = 5. [[0, 3], [1, 2]]
+     * @param arrList
+     * @param tgt
+     * @return
+     */
+    public static List<ArrayList<Integer>> findAddInsIndex(ArrayList<Integer> arrList, int tgt) {
+        List<ArrayList<Integer>> outputList = new ArrayList<>();
+        for(int num : arrList) {
+            if (arrList.indexOf(tgt - num) != -1) {
+                ArrayList<Integer> addList = new ArrayList<>();
+                addList.add(arrList.indexOf(num));
+                addList.add(arrList.indexOf(tgt - num));
+                outputList.add(addList);
+            }
+        }
+        return outputList;
+    }
+    public static List<ArrayList<Integer>> findAddIns(ArrayList<Integer> arrList, int tgt) {
+        List<ArrayList<Integer>> outputList = new ArrayList<>();
+        for(int num : arrList) {
+            if (arrList.indexOf(tgt - num) != -1) {
+                ArrayList<Integer> addList = new ArrayList<>();
+                addList.add(arrList.get(arrList.indexOf(num)));
+                addList.add(arrList.get(arrList.indexOf(tgt - num)));
+                outputList.add(addList);
+            }
+        }
+        return outputList;
+    }
+    /**
+     * Daily coding problem from the mailing list. take input array. multiply each index except the current index. save that as the value for that index on a new arr. return the new arr
+     */
+    public static int[] multElems(int[] arr) {
+        int[] output = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            int j = 0;
+            int num = 1;
+            while(j < arr.length) {
+                if(i != j) {
+                    num *= arr[j];
+                }
+                j++;
+            }
+            output[i] = num;
+        }
+        return output;
+    }
+    public static ArrayList<Integer> factors(int num) {
+        ArrayList<Integer> output = new ArrayList<>();
+        int i = 1;
+        while(i <= num) {
+            if(num % i == 0) {
+                output.add(i);
+            }
+            i++;
+        }
+        return output;
     }
 }
--- src/test/java/PracticeTest.java
+++ src/test/java/PracticeTest.java
@@ -1,6 +1,20 @@
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 public class PracticeTest {
+    private ArrayList<Integer> intList;
+    private int[] testArr1;
+    @Before
+    public void setup() {
+        intList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        testArr1 = new int[]{3, 2, 1};
+    }
     @Test
     public void testIfCompanyNameIsEquals() {
         String expected = "Codeup";
@@ -30,4 +44,37 @@ public class PracticeTest {
         //Less Than
         assertEquals("If a is greater than b return the String 'a > b'", "5 > 4", Practice.comp(5, 4));
     }
+    @Test
+    public void testFindAddEnds() {
+        ArrayList<ArrayList<Integer>> resultList = new ArrayList<>();
+        ArrayList<Integer> addList = new ArrayList<>();
+        addList.add(0);
+        addList.add(3);
+        resultList.add(addList);
+        addList = new ArrayList<>();
+        addList.add(1);
+        addList.add(2);
+        resultList.add(addList);
+        addList = new ArrayList<>();
+        addList.add(2);
+        addList.add(1);
+        resultList.add(addList);
+        addList = new ArrayList<>();
+        addList.add(3);
+        addList.add(0);
+        resultList.add(addList);
+        assertEquals(resultList,Practice.findAddInsIndex(intList, 5));
+//        assertEquals(resultList,Practice.findAddIns(intList, 5));
+    }
+    @Test
+    public void testMultElems() {
+        assertArrayEquals(new int[] {2, 3, 6}, Practice.multElems(testArr1));
+    }
+
+    @Test
+    public void testFactors() {
+        intList = new ArrayList<>(Arrays.asList(new Integer[]{1, 2, 3, 6}));
+        assertEquals(intList, Practice.factors(6));
+    }
+
 }
